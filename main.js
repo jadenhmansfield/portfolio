7ad14da92884d46f9ba0f38b86b6d6fdce0f6dd6
jadenhmansfield <91288105+jadenhmansfield@users.noreply.github.com>
2022-11-13 16:41:00 -0500

    check?

--- main.js
+++ main.js
@@ -47,19 +47,14 @@ function scrollRotate() {
 
 const text = document.querySelector(".text-anim");
 const strText = text.textContent;
-
-//split string into an array
 const textArr = strText.split("");
 
-//the span in line 11-13 repeats this string. To get rid of it,...
 text.textContent = "";
 
-//create spans for each letter of the string
 for (let i = 0; i < textArr.length; i++){
     text.innerHTML += "<span>" + textArr[i] + "</span>";
 }
 
-//every 50 milliseconds, add class to each letter
 let char = 0;
 let timer = setInterval(onTick, 500);
 
@@ -73,7 +68,6 @@ function onTick(){
     }
 }
 
-//once animation is complete, stop the timer.
 function animComplete(){
     clearInterval(timer);
     timer = null;
