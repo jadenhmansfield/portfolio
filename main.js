4e3097ff71fd9010249aabf798309250d3855ab0
jadenhmansfield <91288105+jadenhmansfield@users.noreply.github.com>
2022-11-13 16:04:49 -0500

    animation

--- main.js
+++ main.js
@@ -32,6 +32,7 @@ var cursor = {
   cursor.func();
 
 //flower rotate on scroll
+
 window.onscroll = function () {
   scrollRotate();
 };
@@ -39,6 +40,43 @@ window.onscroll = function () {
 function scrollRotate() {
   let image = document.getElementById('flower');
   image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
+}
+
+//text animation preloader
+//credits to marjorie
+
+const text = document.querySelector(".text-anim");
+const strText = text.textContent;
+
+//split string into an array
+const textArr = strText.split("");
+
+//the span in line 11-13 repeats this string. To get rid of it,...
+text.textContent = "";
+
+//create spans for each letter of the string
+for (let i = 0; i < textArr.length; i++){
+    text.innerHTML += "<span>" + textArr[i] + "</span>";
+}
+
+//every 50 milliseconds, add class to each letter
+let char = 0;
+let timer = setInterval(onTick, 500);
+
+function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    if (char === textArr.length){
+        animComplete();
+        return;
+    }
+}
+
+//once animation is complete, stop the timer.
+function animComplete(){
+    clearInterval(timer);
+    timer = null;
 }
 
 //watering can on hover over flower image
